4bf9ea8c820f76bd94d75f0172d30908b60fed16
Dalton Claybrook <daltonclaybrook@gmail.com>
2021-10-26 21:49:53 -0400

    Changed names of targets

--- Package.swift
+++ Package.swift
@@ -5,17 +5,17 @@ import PackageDescription
 let package = Package(
     name: "Clangler",
     products: [
-        .executable(name: "Clangler", targets: ["Clangler"]),
-        .library(name: "ClanglerKit", targets: ["ClanglerKit"])
+        .executable(name: "ClanglerClient", targets: ["ClanglerClient"]),
+        .library(name: "Clangler", targets: ["Clangler"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
-        .target(name: "Clangler", dependencies: [
+        .target(name: "ClanglerClient", dependencies: [
             .product(name: "ArgumentParser", package: "swift-argument-parser"),
         ]),
-        .target(name: "ClanglerKit"),
+        .target(name: "Clangler"),
         .testTarget(name: "ClanglerTests", dependencies: ["Clangler"]),
     ]
 )
